8691376bcf839148a362ad475c2a730e3bb4c7af
MMutanu <melody.mutanu@gmail.com>
2023-01-12 03:15:23 -0800

    minor edits

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 def uniq_add(my_list=[]):
+    new = set(my_list)
     res = 0
-    for value in set(my_list):
-        res += value
-    return (res)
+    for i in new:
+        res += i
+    return res
